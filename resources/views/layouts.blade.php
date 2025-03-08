60511386882c5891ec25db69692136adaa15a025
Ruchira Kaveesha <ruchirakaveesha2020@gmail.com>
2025-03-08 23:06:47 +0530

    initial commit

--- resources/views/layouts.blade.php
+++ resources/views/layouts.blade.php
@@ -5,8 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title> @yield('title','Custom Auth Laravel' )</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" >
+    <script src="https://cdn.tailwindcss.com"></script>
   </head>
-  <body>
+  <body class="bg-gray-900 text-black">
     @yield('content')
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" ></script>
   </body>
