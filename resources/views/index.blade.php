3d5875505fe70483a014a08b8d20ded3f41aa599
Ruchira Kaveesha <ruchirakaveesha2020@gmail.com>
2025-03-09 22:45:43 +0530

    initial commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,8 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>NextGen Coumputing</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" >
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>
+<body class="bg-gray-900 text-black">
     @include('include.header')
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" ></script>
 </body>
 </html>
