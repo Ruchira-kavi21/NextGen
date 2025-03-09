2c869318d4f9ef87b589d5ec8a501d073bd7da77
Ruchira Kaveesha <ruchirakaveesha2020@gmail.com>
2025-03-09 22:30:25 +0530

    nav bar

--- resources/views/include/header.blade.php
+++ resources/views/include/header.blade.php
@@ -1,36 +1,69 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-      <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
+<nav class="shadow-md text-white">
+    <div class="max-w-screen-xl mx-auto px-4 py-2.5">
+        <div class="flex items-center justify-between">
+            <a class="text-2xl font-bold" href="/index">NextGen Coumputing</a>
+
+            <button class="lg:hidden p-2 hover:bg-gray-950 rounded-md focus:outline-none" aria-controls="navbarNav" aria-expanded="false" id="navbar-toggle">
+                <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M3 5h18a1 1 0 011 1v2a1 1 0 01-1 1H3a1 1 0 01-1-1V6a1 1 0 011-1zM3 12h18a1 1 0 011 1v2a1 1 0 01-1 1H3a1 1 0 01-1-1v-2a1 1 0 011-1zM3 19h18a1 1 0 011 1v2a1 1 0 01-1 1H3a1 1 0 01-1-1v-2a1 1 0 011-1z"></path>
+                </svg>
+            </button>
+
+            <div class="hidden lg:flex space-x-8">
+                <a class="hover:text-blue-500" href="/index">Home</a>
+                <a class="hover:text-blue-500" href="#">Components</a>
+                <a class="hover:text-blue-500" href="#">Market</a>
+
+                <div class="relative group">
+                    <button class="hover:text-blue-500 flex items-center">Profile Management
+                        <svg class="w-4 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M6 9l6 6 6-6"></path>
+                        </svg>
+                    </button>
+                    
+                    <div class="absolute hidden bg-gray-900 shadow-md rounded-lg mt-2 w-48 group-hover:block">
+                        <a class="block px-4 py-2 hover:bg-gray-950" href="/login">Login</a>
+                        <a class="block px-4 py-2 hover:bg-gray-950" href="#">Logout</a>
+                        <a class="block px-4 py-2 hover:bg-gray-950" href="#">Profile</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="lg:hidden collapse" id="navbarNav">
+        <ul class="space-y-4">
+            <li><a class="block py-2 px-4 hover:bg-gray-950" href="#">Home</a></li>
+            <li><a class="block py-2 px-4 hover:bg-gray-950" href="#">Components</a></li>
+            <li><a class="block py-2 px-4 hover:bg-gray-950" href="#">Market</a></li>
+            <li class="relative">
+            <button class="hover:text-blue-500 w-full flex items-center justify-between">
+                Profile Management
+                <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M6 9l6 6 6-6"></path>
+                </svg>
+            </button>
+            <div class="absolute hidden bg-gray-900 shadow-md rounded-lg mt-2 w-48">
+                <a class="block px-4 py-2 hover:bg-gray-950" href="/login">Login</a>
+                <a class="block px-4 py-2 hover:bg-gray-950" href="#">Logout</a>
+                <a class="block px-4 py-2 hover:bg-gray-950" href="#">Profile</a>
+            </div>
+            </li>
+        </ul>
+        </div>
     </div>
-  </div>
 </nav>
+<script>
+const toggleButton = document.getElementById('navbar-toggle');
+const navbarMenu = document.getElementById('navbarNav');
+
+toggleButton.addEventListener('click', () => {
+    navbarMenu.classList.toggle('collapse');
+});
+
+const dropdownButton = document.querySelectorAll('.relative');
+dropdownButton.forEach(button => {
+    button.addEventListener('click', () => {
+    const dropdownMenu = button.querySelector('div');
+    dropdownMenu.classList.toggle('hidden');
+    });
+});
+</script>
